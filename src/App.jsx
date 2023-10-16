c87aed1017cfbc48d40158b37b2d3cc2a1a5ee88
Naiim <saidi.naim123@gmail.com>
2023-10-16 22:31:50 +0200

    add api in env and error message char

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,17 @@ import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
 import axios from 'axios';
 import { BiCurrentLocation, BsSearch } from 'react-icons/all';
-import DarkMode from './components/DarkMode';
 
 function App() {
   const [data, setData] = useState({});
   const [location, setLocation] = useState('');
+  const [errorMessage, setErrorMessage] = useState(false);
 
   const searchLocation = async (event) => {
     if (event.key === 'Enter') {
       event.preventDefault();
-      // console.log(event);
-      const url = `http://api.weatherapi.com/v1/forecast.json?key=fead2aef88e342489ed130154231804&q=${location}&days=6&aqi=no&alerts=no&lang=fr`;
+
+      const url = `http://api.weatherapi.com/v1/forecast.json?key=${import.meta.env.VITE_API_KEY}&q=${location}&days=6&aqi=no&alerts=no&lang=fr`;
 
       //   setLoading(true);
       try {
@@ -33,11 +33,12 @@ function App() {
   });
 
   const fetchWeatherData = async () => {
-    const url = `http://api.weatherapi.com/v1/forecast.json?key=fead2aef88e342489ed130154231804&q=${userLocation.latitude},${userLocation.longitude}&days=6&aqi=no&alerts=no&lang=fr`;
+    const url = `http://api.weatherapi.com/v1/forecast.json?key=${import.meta.env.VITE_API_KEY}&q=${userLocation.latitude},${
+      userLocation.longitude
+    }&days=7&aqi=no&alerts=no&lang=fr`;
     const response = await fetch(url);
     const data = await response.json();
     setData(data);
-    // console.log(data);
     setLocation(data.name);
   };
 
@@ -45,12 +46,10 @@ function App() {
     if (userLocation.latitude && userLocation.longitude) {
       fetchWeatherData();
     }
-    // eslint-disable-next-line
   }, [userLocation]);
 
   const handleUserLocation = () => {
     if (navigator.geolocation) {
-      console.log('test');
       navigator.geolocation.getCurrentPosition((position) => {
         setUserLocation({
           latitude: position.coords.latitude,
@@ -74,15 +73,25 @@ function App() {
       items-center
       '
     >
-      {/* <DarkMode /> */}
       <div className='flex flex-col mt-5 md:mt-5 formContainer w-max'>
         <form className='flex justify-center relative'>
           <input
-            className='rounded-[8px] px-4 py-3 uppercase w-70 sm:w-96 items-center focus:placeholder-transparent focus:outline-[#ff9800] focus:outline-1'
+            className='rounded-[8px] px-4 py-3 capitalize w-70 sm:w-96 items-center focus:placeholder-transparent focus:outline-[#ff9800] focus:outline-1'
             type='text'
             placeholder='Enter Location'
             value={location}
-            onChange={(e) => setLocation(e.target.value)}
+            onChange={(e) => {
+              const inputValue = e.target.value;
+              const specialCharactersRegex = /[^a-zA-Z0-9\s]/;
+              if (specialCharactersRegex.test(inputValue)) {
+                // Affichez un message d'erreur ou effectuez une action appropriée
+                setErrorMessage(true);
+                console.log('Caractères spéciaux détectés. Veuillez entrer un emplacement valide.');
+              } else {
+                // Mettez à jour la valeur de l'emplacement
+                setLocation(inputValue);
+              }
+            }}
             onKeyDown={(e) => {
               if (e.key === 'Enter') {
                 e.preventDefault();
@@ -90,6 +99,7 @@ function App() {
               }
             }}
           />
+
           <button type='button' className='absolute right-11 top-3 w-[55px] text-[#ff9800] ' onClick={handleUserLocation}>
             <BiCurrentLocation className='text-2xl' />
           </button>
@@ -98,23 +108,25 @@ function App() {
           </button>
         </form>
       </div>
+      {errorMessage ? <div className='text-red-400 py-2'>Caractères spéciaux détectés.</div> : null}
 
       {Object.keys(data).length !== 0 && (
         <div className='grid gap-5'>
           <div className=' flex justify-center'>
             <div className='card col-start-1 rounded-lg justify-items-center text-white w-[400px] h-[350px] grid grid-cols-2 items-center mt-[-8px] mb-[35px]'>
               <img src={data?.current.condition.icon} alt='logo weather' className='w-[150px] self-center' />
-              <h2 className='text-8xl col-start-1 '>{data?.current.temp_c}&deg;c</h2>
-              <h4 className='text-xl font-bold text-center col-start-1'>
+              <h1 className='text-8xl col-start-1 '>{data?.current.temp_c}&deg;c</h1>
+              <h2 className='text-xl font-bold text-center col-start-1'>
                 {data?.location.name}, {data?.location.country}
-              </h4>
+              </h2>
               <p className='col-start-1'>{data?.current.condition.text}</p>
+              <p className='col-start-2'>Feels: {data?.current.feelslike_c}</p>
             </div>
           </div>
           <div className='ml-2 mr-2 grid text-center grid-cols-1 align-center lg:grid-cols-2 xl:grid-cols-3 md:grid-cols-2 gap-7 row-start-2 grid-flow-dense'>
             {data.forecast.forecastday.slice(0).map((day) => (
               <div key={day.date} className='containerForecast flex justify-center'>
-                <div className='flex flex-col mb-[15px] w-[200px] h-[200px] rounded-lg  bg-[#ff9800] text-white mt-[20px] '>
+                <div className='flex flex-col mb-[15px] w-[200px] h-[200px] rounded-lg  bg-[#f2f2f2a7] backdrop-blur- text-white mt-[20px] '>
                   <h1>{formatDate(day.date)}</h1>
                   <img className='w-[85px] self-center' src={day?.day.condition.icon} alt='logo weather' />
                   <h2 className='justify-start font-bold text-2xl '>{Math.round(day.day.avgtemp_c)}&deg;c</h2>
