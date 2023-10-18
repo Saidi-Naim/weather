c1d594b29e57916651aedf5dc37e3a9a7b9d7cd2
Naiim <saidi.naim123@gmail.com>
2023-10-18 23:12:18 +0200

    change style forecast

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -13,7 +13,7 @@ const Forecast = ({ dayData, data, setDayData }) => {
     <div className=' ml-2 mr-2 flex justify-center flex-wrap text-center align-center lg:grid-cols-2 xl:grid-cols-3 md:grid-cols-2 gap-7 row-start-2 grid-flow-dense'>
       {data?.forecast?.forecastday.slice(0).map((day) => (
         <div onClick={() => handleDay(day)} key={day.date} className='containerForecast flex justify-center'>
-          <div className='flex flex-col mb-[15px] w-[200px] h-[200px] rounded-lg  bg-[#f2f2f2a7] backdrop-blur- text-white mt-[20px] '>
+          <div className='flex flex-col mb-[15px] w-[200px] h-[200px] rounded-lg  bg-[#f2f2f215] backdrop-blur-2xl text-white mt-[20px] '>
             <h1>{formatDate(day.date)}</h1>
             <img className='w-[85px] self-center' src={day?.day.condition.icon} alt='logo weather' />
             <h2 className='justify-start font-bold text-2xl '>{Math.round(day.day.avgtemp_c)}&deg;c</h2>
